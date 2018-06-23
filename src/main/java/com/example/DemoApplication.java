97c5f288e0018a6e4d9ff5f689113773cfe5938e
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2018-06-23 01:53:50 -0400

    probNDO

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,13 +6,24 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 @ComponentScan({"com.example.demo", "com.example.Controllers"})
 @EntityScan("com.example.Entities")
 @EnableJpaRepositories("com.example.Repositories")
-public class DemoApplication {
+public class DemoApplication   extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
 }
