15057593f00656957519b3942cadc07029f80bdd
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2018-06-25 04:24:09 -0400

    carpeta demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,24 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 @SpringBootApplication
 @ComponentScan({"com.example.demo", "com.example.Controllers"})
 @EntityScan("com.example.Entities")
 @EnableJpaRepositories("com.example.Repositories")
-public class DemoApplication   extends SpringBootServletInitializer{
+public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DemoApplication.class);
-    }
-
 }
